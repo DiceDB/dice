79d2a6cefc04fd4d918cd06c50f9ea5094955b13
Keshav Chand <chandkeshav001@gmail.com>
2022-11-09 21:08:10 +0530

    Create aof with correct permission

--- core/aof.go
+++ core/aof.go
@@ -20,11 +20,13 @@ func dumpKey(fp *os.File, key string, obj *Obj) {
 
 // TODO: To to new and switch
 func DumpAllAOF() {
-	fp, err := os.OpenFile(config.AOFFile, os.O_CREATE|os.O_WRONLY, os.ModeAppend)
+	// If Not present create append only file with rw-r-r permission
+	fp, err := os.OpenFile(config.AOFFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Print("error", err)
 		return
 	}
+	defer fp.Close()
 	log.Println("rewriting AOF file at", config.AOFFile)
 	for k, obj := range store {
 		dumpKey(fp, k, obj)
