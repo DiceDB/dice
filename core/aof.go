c701083344d2c7b11594cb8c8d854aa4f8600d32
Rohan Verma <rohan40594@gmail.com>
2022-11-09 23:17:56 +0530

    safe file operation & error handling for the same

--- core/aof.go
+++ core/aof.go
@@ -25,9 +25,25 @@ func DumpAllAOF() {
 		fmt.Print("error", err)
 		return
 	}
+	
+	/* Note: A close function also returns an error, a plain defer is harmful. 
+	A successful close does not guarantee that the data has been successfully saved 
+	to disk,as the kernel uses the buffer cache to defer writes or write calls delays the writing 
+	to disk to mitigate cost of frequent writes to disk. A more reliable method is is to use 
+	fsync() or f.Sync() and Direct I/O
+	*/
+	defer func() {
+	if err := fp.Close(); err != nil {
+		fmt.Print("error", err)
+		return
+	}
+	}()
+
 	log.Println("rewriting AOF file at", config.AOFFile)
 	for k, obj := range store {
 		dumpKey(fp, k, obj)
 	}
+	
+
 	log.Println("AOF file rewrite complete")
 }
