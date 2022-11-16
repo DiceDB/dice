0fd11546cf973a43e06203ac043a292b5c38db87
FuncGuy <tim25659@gmail.com>
2022-11-16 11:20:59 +0530

    Addressed the review comments-2

--- tests/incr_test.go
+++ tests/incr_test.go
@@ -8,6 +8,7 @@ import (
 
 type tcase struct {
 	op  string
+	key string
 	val int64
 }
 
@@ -17,27 +18,38 @@ func TestINCR(t *testing.T) {
 	conn := getLocalConnection()
 
 	for _, tc := range []tcase{
-		{"s1", 0},
-		{"i1", 1},
-		{"i1", 1},
+		{"s", "key1", 0},
+		{"i", "key1", 1},
+		{"i", "key1", 1},
+		{"i", "key2", 1},
 	} {
 
 		switch tc.op[0] {
 		case 's':
-			cmd := fmt.Sprintf("SET Key %d", tc.val)
+			cmd := fmt.Sprintf("SET %s %d", tc.key, tc.val)
 			fireCommand(conn, cmd)
 		case 'i':
-			cmd := fmt.Sprintf("INCR Key")
+			cmd := fmt.Sprintf("INCR %s", tc.key)
 			fireCommand(conn, cmd)
 		}
 	}
 
-	cmd := fmt.Sprintf("GET Key")
+	cmd := fmt.Sprintf("GET key1")
 	r := fireCommand(conn, cmd)
-	if r != "2" {
-		t.Fail()
-	}
+	expected := "2"
+	assertResult(t, r, expected)
+
+	cmd = fmt.Sprintf("GET key2")
+	r = fireCommand(conn, cmd)
+	expected = "1"
+	assertResult(t, r, expected)
 
 	fireCommand(conn, "ABORT")
 	wg.Wait()
+}
+
+func assertResult(t *testing.T, r interface{}, expected string) {
+	if r != expected {
+		t.Fail()
+	}
 }
