8e07d003ef3cb5172eff75d88349e32174fce1d7
Jyotinder Singh <jyotindrsingh@gmail.com>
2022-11-29 20:05:13 +0530

    Added StackElement encoding

--- core/resp.go
+++ core/resp.go
@@ -173,6 +173,23 @@ func Encode(value interface{}, isSimple bool) []byte {
 			buf.Write(Encode(qe.Obj.Value, false))
 		}
 		return []byte(fmt.Sprintf("*%d\r\n%s", len(elements), buf.Bytes()))
+	case *StackElement:
+		var b []byte
+		buf := bytes.NewBuffer(b)
+		se := value.(*StackElement)
+		buf.Write(Encode(se.Key, false))
+		buf.Write(Encode(se.Obj.Value, false))
+		return []byte(fmt.Sprintf("*2\r\n%s", buf.Bytes()))
+	case []*StackElement:
+		var b []byte
+		buf := bytes.NewBuffer(b)
+		elements := value.([]*StackElement)
+		for _, se := range elements {
+			buf.Write([]byte("*2\r\n"))
+			buf.Write(Encode(se.Key, false))
+			buf.Write(Encode(se.Obj.Value, false))
+		}
+		return []byte(fmt.Sprintf("*%d\r\n%s", len(elements), buf.Bytes()))
 	case []int64:
 		var b []byte
 		buf := bytes.NewBuffer(b)
