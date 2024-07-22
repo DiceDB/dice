a9fe1752a629f0e067837bf1e49699e7f0283c27
Jyotinder Singh <jyotindrsingh@gmail.com>
2024-07-22 06:34:54 +0530

    Ensures thread-safe access to store and keypool (#173)
    * ensure thread-safe access to store and keypool
    
    * Added comment to guide ordered locking

--- core/store.go
+++ core/store.go
@@ -20,7 +20,13 @@ var expires map[*Obj]uint64 // Does not need to be thread-safe as it is only acc
 var keypool map[string]unsafe.Pointer
 var WatchList sync.Map // Maps queries to the file descriptors of clients that are watching them.
 
-// WatchChannel Channel to receive updates about keys that are being watched.
+var storeMutex sync.RWMutex   // Mutex to protect the store map, must be acquired before keypoolMutex if both are needed.
+var keypoolMutex sync.RWMutex // Mutex to protect the keypool map, must be acquired after storeMutex if both are needed.
+
+// Channel to receive updates about keys that are being watched.
+// The Watcher goroutine will wait on this channel. When a key is updated, the
+// goroutine will send the updated value and the related operation to all the
+// clients that are watching the key.
 var WatchChannel chan WatchEvent
 
 func NewStore() *Store {
