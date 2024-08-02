2029444c51f0adf426d28339cf20e1d8d383ae69
Sameer Kolhar <kolhar730@gmail.com>
2024-08-02 13:35:48 -0700

    temp: add test to set_test.go

--- tests/set_test.go
+++ tests/set_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"testing"
+	"time"
 
 	"gotest.tools/v3/assert"
 )
@@ -18,6 +19,32 @@ func TestSet(t *testing.T) {
 			cmd := tcase.InCmds[i]
 			out := tcase.Out[i]
 			assert.Equal(t, out, fireCommand(conn, cmd), "Value mismatch for cmd %s\n.", cmd)
+		}
+	}
+}
+
+func TestSetWithKeepTTLFlag(t *testing.T) {
+	conn := getLocalConnection()
+	for _, tcase := range []DTestCase{
+		{
+			InCmds: []string{"SET k v EX 6"},
+			Out:    []interface{}{"OK"},
+		},
+		{
+			InCmds: []string{"SET k vv KEEPTTL", "GET k"},
+			Out:    []interface{}{"OK", "vv"},
+		},
+		{
+			InCmds: []string{"GET k"},
+			Out:    []interface{}{"nil"},
+		},
+	} {
+		for i := 0; i < len(tcase.InCmds); i++ {
+			cmd := tcase.InCmds[i]
+			out := tcase.Out[i]
+			// fmt.Println(fireCommand(conn, cmd))
+			assert.Equal(t, out, fireCommand(conn, cmd), "Value mismatch for cmd %s\n.", cmd)
+			time.Sleep(3 * time.Second)
 		}
 	}
 }
