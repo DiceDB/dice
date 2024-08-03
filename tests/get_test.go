8979a5ad92f49a19da36740d6ab21d9ccb2218d1
ayadav16 <ayadav7@binghamton.edu>
2024-08-03 18:25:20 -0400

    added delay instead of sleep

--- tests/get_test.go
+++ tests/get_test.go
@@ -2,20 +2,31 @@ package tests
 
 import (
 	"testing"
+	"time"
 
 	"gotest.tools/v3/assert"
 )
 
+type DDelayTestCase struct {
+	InCmds []string
+	Out    []interface{}
+	Delay  []time.Duration
+}
+
 func TestGet(t *testing.T) {
 	conn := getLocalConnection()
 	defer conn.Close()
-	for _, tcase := range []DTestCase{
+	for _, tcase := range []DDelayTestCase{
 		{
-			InCmds: []string{"SET k v EX 4", "GET k", "SLEEP 5", "GET k"},
-			Out:    []interface{}{"OK", "v", "OK", "(nil)"},
+			InCmds: []string{"SET k v EX 4", "GET k", "GET k"},
+			Out:    []interface{}{"OK", "v", "(nil)"},
+			Delay:  []time.Duration{0, 0, 5 * time.Second},
 		},
 	} {
 		for i := 0; i < len(tcase.InCmds); i++ {
+			if tcase.Delay[i] > 0 {
+				time.Sleep(tcase.Delay[i])
+			}
 			cmd := tcase.InCmds[i]
 			out := tcase.Out[i]
 			assert.Equal(t, out, fireCommand(conn, cmd), "Value mismatch for cmd %s\n.", cmd)
