f69d59b8ccf68d8b40b6c795ab148f55757a5d59
kushal0511-not <77385889+kushal0511-not@users.noreply.github.com>
2024-08-10 16:50:37 +0530

    #291: Add DECRBY support (#294)

--- tests/decr_test.go
+++ tests/decr_test.go
@@ -65,3 +65,81 @@ func TestDECR(t *testing.T) {
 		})
 	}
 }
+
+func TestDECRBY(t *testing.T) {
+	conn := getLocalConnection()
+	defer conn.Close()
+
+	type SetCommand struct {
+		key string
+		val int64
+	}
+
+	type DecrByCommand struct {
+		key         string
+		decrValue   any
+		expectedVal int64
+		expectedErr string
+	}
+
+	type GetCommand struct {
+		key         string
+		expectedVal int64
+	}
+
+	testCases := []struct {
+		name           string
+		setCommands    []SetCommand
+		decrByCommands []DecrByCommand
+		getCommands    []GetCommand
+	}{
+		{
+			name: "Decrement multiple keys",
+			setCommands: []SetCommand{
+				{"key1", 3},
+				{"key3", math.MinInt64 + 1},
+			},
+			decrByCommands: []DecrByCommand{
+				{"key1", int64(2), 1, ""},
+				{"key1", int64(1), 0, ""},
+				{"key4", int64(1), -1, ""},
+				{"key3", int64(1), math.MinInt64, ""},
+				{"key3", int64(math.MinInt64), 0, "ERR value is out of range"},
+				{"key5", "abc", 0, "ERR value is not an integer or out of range"},
+			},
+			getCommands: []GetCommand{
+				{"key1", 0},
+				{"key4", -1},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			for _, cmd := range tc.setCommands {
+				fireCommand(conn, fmt.Sprintf("SET %s %d", cmd.key, cmd.val))
+			}
+
+			for _, cmd := range tc.decrByCommands {
+				var result any
+				switch v := cmd.decrValue.(type) {
+				case int64:
+					result = fireCommand(conn, fmt.Sprintf("DECRBY %s %d", cmd.key, v))
+				case string:
+					result = fireCommand(conn, fmt.Sprintf("DECRBY %s %s", cmd.key, v))
+				}
+				switch v := result.(type) {
+				case string:
+					assert.Equal(t, cmd.expectedErr, v)
+				case int64:
+					assert.Equal(t, cmd.expectedVal, v)
+				}
+			}
+
+			for _, cmd := range tc.getCommands {
+				result := fireCommand(conn, fmt.Sprintf("GET %s", cmd.key))
+				assert.Equal(t, strconv.FormatInt(cmd.expectedVal, 10), result)
+			}
+		})
+	}
+}
