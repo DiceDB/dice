5e28c4edcdb440487c7a2a094ed10fcfaf655289
Ritik Jain <ritikjain1272@gmail.com>
2024-08-11 19:26:11 +0530

    added test case for multiple keys DEL operation

--- tests/del_test.go
+++ tests/del_test.go
@@ -21,15 +21,20 @@ func TestDel(t *testing.T) {
 			expected: []interface{}{"OK", int64(1)},
 		},
 		{
+			name:     "DEL with multiple keys",
+			commands: []string{"SET k1 v1", "SET k2 v2", "DEL k1 k2"},
+      expected: []interface{}{"OK", "OK", int64(2)},
+		},
+		{
 			name:     "DEL with key not set",
-			commands: []string{"DEL k2"},
+			commands: []string{"DEL k3"},
 			expected: []interface{}{int64(0)},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			deleteTestKeys([]string{"k1", "k2"})
+      deleteTestKeys([]string{"k1", "k2", "k3"})
 			for i, cmd := range tc.commands {
 				result := fireCommand(conn, cmd)
 				assert.Equal(t, tc.expected[i], result, "Value mismatch for cmd %s", cmd)
