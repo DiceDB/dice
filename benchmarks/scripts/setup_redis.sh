1e7ce13a85659fbc79a3593c3fbc9ef6556acdfb
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2024-08-12 16:22:19 +0530

    Deleting scripts for benchmarks, as they moved to exo

--- benchmarks/scripts/setup_redis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/redis/redis/
-cd redis
-git checkout 7.2.5
-make
-cd src
-nohup ./redis-server > /dev/null 2>&1 &
--- benchmarks/scripts/stop_redis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pkill -9 redis-server
