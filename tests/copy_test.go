6ea04302f73ebb1a51f6815e73e4e5ca97df52a6
srivastava-yash <yash.184040@gmail.com>
2024-08-12 14:21:33 -0400

    tests refractor

--- tests/copy_test.go
+++ tests/copy_test.go
@@ -19,7 +19,7 @@ func TestCopy(t *testing.T) {
 	}{
 		{
 			name:     "COPY when source key doesn't exist",
-			commands: []string{"COPY k3 k2"},
+			commands: []string{"COPY k1 k2"},
 			expected: []interface{}{int64(0)},
 		},
 		{
@@ -29,34 +29,34 @@ func TestCopy(t *testing.T) {
 		},
 		{
 			name:     "COPY with REPLACE",
-            commands: []string{"SET k4 v1", "SET k5 v2", "GET k5", "COPY k4 k5 REPLACE", "GET k5"},
+			commands: []string{"SET k1 v1", "SET k2 v2", "GET k2", "COPY k1 k2 REPLACE", "GET k2"},
 			expected: []interface{}{"OK", "OK", "v2", int64(1), "v1"},
 		},
-        {
-            name: "COPY with JSON integer",
-            commands: []string{"JSON.SET k6 $ 2", "COPY k6 k7", "JSON.GET k7"},
-            expected: []interface{}{"OK", int64(1), "2"},
-        },
-        {
-            name: "COPY with JSON boolean",
-            commands: []string{"JSON.SET k8 $ true", "COPY k8 k9", "JSON.GET k8"},
-            expected: []interface{}{"OK", int64(1), "true"},
-        },
-        {
-            name: "COPY with JSON array",
-            commands: []string{`JSON.SET k10 $ [1,2,3]`, "COPY k10 k11", "JSON.GET k11"},
-            expected: []interface{}{"OK", int64(1), `[1,2,3]`},
-        },
-        {
-            name: "COPY with JSON simple JSON with REPLACE",
-            commands: []string{`JSON.SET k12 $ ` + simpleJSON, "COPY k12 k11 REPLACE", "JSON.GET k11"},
-            expected: []interface{}{"OK", int64(1), simpleJSON},
-        },
+		{
+			name:     "COPY with JSON integer",
+			commands: []string{"JSON.SET k1 $ 2", "COPY k1 k2", "JSON.GET k2"},
+			expected: []interface{}{"OK", int64(1), "2"},
+		},
+		{
+			name:     "COPY with JSON boolean",
+			commands: []string{"JSON.SET k1 $ true", "COPY k1 k2", "JSON.GET k2"},
+			expected: []interface{}{"OK", int64(1), "true"},
+		},
+		{
+			name:     "COPY with JSON array",
+			commands: []string{`JSON.SET k1 $ [1,2,3]`, "COPY k1 k2", "JSON.GET k2"},
+			expected: []interface{}{"OK", int64(1), `[1,2,3]`},
+		},
+		{
+			name:     "COPY with JSON simple JSON with REPLACE",
+			commands: []string{`JSON.SET k1 $ ` + simpleJSON, "COPY k1 k2 REPLACE", "JSON.GET k2"},
+			expected: []interface{}{"OK", int64(1), simpleJSON},
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			deleteTestKeys([]string{"k1", "k2", "k3", "k4", "k5", "k6", "k7", "k8", "k9", "k10", "k11"})
+			deleteTestKeys([]string{"k1", "k2"})
 			for i, cmd := range tc.commands {
 				result := fireCommand(conn, cmd)
 				assert.Equal(t, tc.expected[i], result, "Value mismatch for cmd %s", cmd)
