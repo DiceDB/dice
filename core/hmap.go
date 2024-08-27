3ccf4ea6eb215f6c874b7a13a646ce6e3010efde
Sameer Kolhar <kolhar730@gmail.com>
2024-08-26 22:28:31 -0400

    update hmap.go based review comments

--- core/hmap.go
+++ core/hmap.go
@@ -2,38 +2,81 @@ package core
 
 import (
 	"errors"
-
-	orderedmap "github.com/wk8/go-ordered-map/v2"
 )
 
-type HMap = *orderedmap.OrderedMap[string, string]
+type HashMap map[string]string
 
-func hashMapBuilder(args []string, currentHashMap HMap) (HMap, int64, error) {
-	var hmap HMap
+func (h HashMap) Get(k string) (*string, bool) {
+	value, ok := h[k]
+	if !ok {
+		return nil, false
+	}
+	return &value, true
+}
+
+func (h HashMap) Set(k, v string) (*string, bool) {
+	value, ok := h[k]
+	if ok {
+		oldValue := value
+		h[k] = v
+		return &oldValue, true
+	}
+
+	h[k] = v
+	return nil, false
+}
+
+func hashMapBuilder(keyValuePairs []string, currentHashMap HashMap) (HashMap, int64, error) {
+	var hmap HashMap
 	var numKeysNewlySet int64
 
 	if currentHashMap == nil {
-		hmap = orderedmap.New[string, string]()
+		hmap = make(HashMap)
 	} else {
 		hmap = currentHashMap
 	}
 
-	iter := 1 // NOTE: to move past args[0] (aka key) within args
+	iter := 0
+	argLength := len(keyValuePairs)
 
-	for iter <= len(args)-1 {
-		if iter < len(args)-1 && iter+1 <= len(args)-1 {
-			k := args[iter]
-			v := args[iter+1]
-
-			_, present := hmap.Set(k, v)
-			if !present {
-				numKeysNewlySet++
-			}
-			iter += 2
-		} else {
-			return hmap, -1, errors.New("ERR mismatch in number of fields and values provided to HSET")
+	for iter <= argLength-1 {
+		if iter >= argLength-1 || iter+1 > argLength-1 {
+			return hmap, -1, errors.New("mismatch in number of fields and values provided to HSET")
 		}
+
+		k := keyValuePairs[iter]
+		v := keyValuePairs[iter+1]
+
+		_, present := hmap.Set(k, v)
+		if !present {
+			numKeysNewlySet++
+		}
+		iter += 2
 	}
 
 	return hmap, numKeysNewlySet, nil
+}
+
+func getValueFromHashMap(key, field string, store *Store) ([]byte, error) {
+	var value string
+
+	obj := store.Get(key)
+
+	if obj == nil {
+		return RespNIL, nil
+	}
+
+	switch currentVal := obj.Value.(type) {
+	case HashMap:
+		val, present := currentVal.Get(field)
+		if !present {
+			return RespNIL, nil
+		}
+		value = *val
+	default:
+		return nil, errors.New("WRONGTYPE Operation against a key holding the wrong kind of value")
+	}
+
+	// return Encode(value, false), nil
+	return []byte(value), nil
 }
