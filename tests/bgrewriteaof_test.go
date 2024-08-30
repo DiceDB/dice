1139cf5a6b520f248d90c6281c0737a0e87d3ce5
ashwaniYDV <ashyadavash@gmail.com>
2024-08-31 02:17:08 +0530

    updated integration tests

--- tests/bgrewriteaof_test.go
+++ tests/bgrewriteaof_test.go
@@ -1,7 +1,10 @@
 package tests
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
+	"time"
 
 	"gotest.tools/v3/assert"
 )
@@ -9,6 +12,10 @@ import (
 func TestBgrewriteaof(t *testing.T) {
 	conn := getLocalConnection()
 	defer conn.Close()
+
+	// Defer file removal to ensure cleanup after test completion
+	// remove the dice-master.aof file created inside tests folder
+	defer os.Remove("dice-master.aof")
 
 	testCases := []TestCase{
 		{
@@ -22,5 +29,21 @@ func TestBgrewriteaof(t *testing.T) {
 			result := fireCommand(conn, cmd)
 			assert.DeepEqual(t, tc.expected[i], result)
 		}
+
+		// ensure that file is written
+		time.Sleep(time.Second*2)
+		
+		fileContent, err := ioutil.ReadFile("dice-master.aof")
+		if err != nil {
+			t.Fatalf("Failed to read the file: %v", err)
+		}
+
+		expectedFileContent, err := ioutil.ReadFile("../testutils/bgrewriteaof-expected-data.aof")
+		if err != nil {
+			t.Fatalf("Failed to read the file: %v", err)
+		}
+		
+		// Assert that the file content matches the expected data
+		assert.Equal(t, string(fileContent), string(expectedFileContent), "File content does not match expected content")
 	}
 }
