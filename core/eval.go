13f179197b50a21171394493049c63060b3aebf6
Kaushik Kakdey <52580667+kakdeykaushik@users.noreply.github.com>
2024-09-03 00:22:47 +0530

    #149: feat: Added COMMAND LIST cmd (#418)

--- core/eval.go
+++ core/eval.go
@@ -2017,9 +2017,19 @@ func evalCommand(args []string, store *Store) []byte {
 		return evalCommandCount()
 	case "GETKEYS":
 		return evalCommandGetKeys(args[1:])
+	case "LIST":
+		return evalCommandList()
 	default:
-		return diceerrors.NewErrWithFormattedMessage("unknown subcommand '%s'. Try COMMAND HELP", subcommand)
+		return diceerrors.NewErrWithFormattedMessage("unknown subcommand '%s'. Try COMMAND HELP.", subcommand)
 	}
+}
+
+func evalCommandList() []byte {
+	cmds := make([]string, 0, diceCommandsCount)
+	for k := range diceCmds {
+		cmds = append(cmds, k)
+	}
+	return Encode(cmds, false)
 }
 
 // evalKeys returns the list of keys that match the pattern
