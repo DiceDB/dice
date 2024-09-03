ed7ae47b0d1e4ee83e9a3ff2424a680d6d5c7709
Prashant Shubham <prashant1996cr07@gmail.com>
2024-09-03 12:37:54 +0530

    #238: PFADD/PFCOUNT implementation (#422)

--- core/commands.go
+++ core/commands.go
@@ -107,7 +107,7 @@ var (
 	jsonclearCmdMeta = DiceCmdMeta{
 		Name: "JSON.CLEAR",
 		Info: `JSON.CLEAR key [path]
-		Returns an integer reply specifying the number ofmatching JSON arrays and 
+		Returns an integer reply specifying the number ofmatching JSON arrays and
 		objects cleared +number of matching JSON numerical values zeroed.
 		Error reply: If the number of arguments is incorrect the key doesn't exist.`,
 		Eval:     evalJSONCLEAR,
@@ -665,22 +665,22 @@ var (
 	bitposCmdMeta = DiceCmdMeta{
 		Name: "BITPOS",
 		Info: `BITPOS returns the position of the first bit set to 1 or 0 in a string
-		 The position is returned, thinking of the string as an array of bits from left to right, 
-		 where the first byte's most significant bit is at position 0, the second byte's most significant 
+		 The position is returned, thinking of the string as an array of bits from left to right,
+		 where the first byte's most significant bit is at position 0, the second byte's most significant
 		 bit is at position 8, and so forth.
-		 By default, all the bytes contained in the string are examined. It is possible to look for bits only in a 
-		 specified interval passing the additional arguments start and end (it is possible to just pass start, 
+		 By default, all the bytes contained in the string are examined. It is possible to look for bits only in a
+		 specified interval passing the additional arguments start and end (it is possible to just pass start,
 		 the operation will assume that the end is the last byte of the string).
-		 By default, the range is interpreted as a range of bytes and not a range of bits, so start=0 and end=2 means 
+		 By default, the range is interpreted as a range of bytes and not a range of bits, so start=0 and end=2 means
 		 to look at the first three bytes.
-		 You can use the optional BIT modifier to specify that the range should be interpreted as a range of bits. So 
+		 You can use the optional BIT modifier to specify that the range should be interpreted as a range of bits. So
 		 start=0 and end=2 means to look at the first three bits.
-		 Note that bit positions are returned always as absolute values starting from bit zero even when start and end 
+		 Note that bit positions are returned always as absolute values starting from bit zero even when start and end
 		 are used to specify a range.
-		 The start and end can contain negative values in order to index bytes starting from the end of the string, 
-		 where -1 is the last byte, -2 is the penultimate, and so forth. When BIT is specified, -1 is the last bit, -2 
+		 The start and end can contain negative values in order to index bytes starting from the end of the string,
+		 where -1 is the last byte, -2 is the penultimate, and so forth. When BIT is specified, -1 is the last bit, -2
 		 is the penultimate, and so forth.
-		 Returns	
+		 Returns
 		 RESP encoded integer indicating the position of the first bit set to 1 or 0 according to the request.
 		 RESP encoded integer if we look for clear bits and the string only contains bits set to 1, the function returns
 	     the first bit not part of the string on the right.
@@ -743,6 +743,22 @@ var (
 		Returns the members of the set resulting from the intersection of all the given sets.
 		Non existing keys are treated as empty sets.`,
 		Eval:     evalSINTER,
+		Arity:    -2,
+		KeySpecs: KeySpecs{BeginIndex: 1},
+	}
+	pfAddCmdMeta = DiceCmdMeta{
+		Name: "PFADD",
+		Info: `PFADD key [element [element ...]]
+		Adds elements to a HyperLogLog key. Creates the key if it doesn't exist.`,
+		Eval:     evalPFADD,
+		Arity:    -2,
+		KeySpecs: KeySpecs{BeginIndex: 1},
+	}
+	pfCountCmdMeta = DiceCmdMeta{
+		Name: "PFCOUNT",
+		Info: `PFCOUNT key [key ...]
+		Returns the approximated cardinality of the set(s) observed by the HyperLogLog key(s).`,
+		Eval:     evalPFCOUNT,
 		Arity:    -2,
 		KeySpecs: KeySpecs{BeginIndex: 1},
 	}
@@ -832,4 +848,6 @@ func init() {
 	diceCmds["SCARD"] = scardCmdMeta
 	diceCmds["SDIFF"] = sdiffCmdMeta
 	diceCmds["SINTER"] = sinterCmdMeta
+	diceCmds["PFADD"] = pfAddCmdMeta
+	diceCmds["PFCOUNT"] = pfCountCmdMeta
 }
