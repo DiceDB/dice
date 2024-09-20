f851d56102718b35bf8755b2134ebf32027e5fc5
Harshit Gupta <harshitgupta1406@gmail.com>
2024-09-20 15:56:29 +0530

    Add integration tests for ECHO command

---
+++ integration_tests/commands/echo_test.go
@@ -0,0 +1,38 @@
+package commands
+
+import (
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestEcho(t *testing.T) {
+	conn := getLocalConnection()
+	defer conn.Close()
+
+	testCases := []struct {
+		name     string
+		commands []string
+		expected []interface{}
+	}{
+		{
+			name:     "ECHO with invalid number of arguments",
+			commands: []string{"ECHO"},
+			expected: []interface{}{"ERR wrong number of arguments for 'echo' command"},
+		},
+		{
+			name:     "ECHO with one argument",
+			commands: []string{"ECHO \"hello world\""},
+			expected: []interface{}{"hello world"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			for i, cmd := range tc.commands {
+				result := FireCommand(conn, cmd)
+				assert.Equal(t, tc.expected[i], result, "Value mismatch for cmd %s", cmd)
+			}
+		})
+	}
+}
