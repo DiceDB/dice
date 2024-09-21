7eae1568edd44420fb5948191de1e1b70afe3092
Ashwin Kulkarni <ashwin.kulkarni128@gmail.com>
2024-09-21 17:10:37 +0530

    Eval refactoring for multithreading (#634)
    * support for eval refactoring for multithreading
    
    * fixed errors
    
    * review comments
    
    * review comments
    
    * eval changes
    
    * fixed multithreading error issue
    
    * linter fixes
    
    * Update eval_test.go
    
    * fixed unit tests
    
    * fix unit tests
    
    * review comments
    
    * more refactoring
    
    * added shard numbers to num core

--- internal/eval/eval.go
+++ internal/eval/eval.go
@@ -44,6 +44,10 @@ var (
 	diceCommandsCount int
 )
 
+type EvalResponse struct {
+	Result interface{} // Result of the Store operation, for now the type is set to []byte, but this can change in the future.
+	Error  error
+}
 type jsonOperation string
 
 const (
@@ -60,26 +64,7 @@ func init() {
 	serverID = fmt.Sprintf("%s:%d", config.DiceConfig.Server.Addr, config.DiceConfig.Server.Port)
 }
 
-// evalPING returns with an encoded "PONG"
-// If any message is added with the ping command,
-// the message will be returned.
-func evalPING(args []string, store *dstore.Store) []byte {
-	var b []byte
-
-	if len(args) >= 2 {
-		return diceerrors.NewErrArity("PING")
-	}
-
-	if len(args) == 0 {
-		b = clientio.Encode("PONG", true)
-	} else {
-		b = clientio.Encode(args[0], false)
-	}
-
-	return b
-}
-
-// evalECHO returns the argument passed by the user 
+// evalECHO returns the argument passed by the user
 func evalECHO(args []string, store *dstore.Store) []byte {
 	if len(args) != 1 {
 		return diceerrors.NewErrArity("ECHO")
@@ -111,127 +96,6 @@ func EvalAUTH(args []string, c *comm.Client) []byte {
 	if err = c.Session.Validate(username, password); err != nil {
 		return clientio.Encode(err, false)
 	}
-	return clientio.RespOK
-}
-
-// evalSET puts a new <key, value> pair in db as in the args
-// args must contain key and value.
-// args can also contain multiple options -
-//
-//	EX or ex which will set the expiry time(in secs) for the key
-//	PX or px which will set the expiry time(in milliseconds) for the key
-//	EXAT or exat which will set the specified Unix time at which the key will expire, in seconds (a positive integer).
-//	PXAT or PX which will the specified Unix time at which the key will expire, in milliseconds (a positive integer).
-//	XX orr xx which will only set the key if it already exists.
-//
-// Returns encoded error response if at least a <key, value> pair is not part of args
-// Returns encoded error response if expiry time value in not integer
-// Returns encoded error response if both PX and EX flags are present
-// Returns encoded OK RESP once new entry is added
-// If the key already exists then the value will be overwritten and expiry will be discarded
-func evalSET(args []string, store *dstore.Store) []byte {
-	if len(args) <= 1 {
-		return diceerrors.NewErrArity("SET")
-	}
-
-	var key, value string
-	var exDurationMs int64 = -1
-	var state exDurationState = Uninitialized
-	var keepttl bool = false
-
-	key, value = args[0], args[1]
-	oType, oEnc := deduceTypeEncoding(value)
-
-	for i := 2; i < len(args); i++ {
-		arg := strings.ToUpper(args[i])
-		switch arg {
-		case Ex, Px:
-			if state != Uninitialized {
-				return diceerrors.NewErrWithMessage(diceerrors.SyntaxErr)
-			}
-			i++
-			if i == len(args) {
-				return diceerrors.NewErrWithMessage(diceerrors.SyntaxErr)
-			}
-
-			exDuration, err := strconv.ParseInt(args[i], 10, 64)
-			if err != nil {
-				return diceerrors.NewErrWithMessage(diceerrors.IntOrOutOfRangeErr)
-			}
-
-			if exDuration <= 0 || exDuration >= maxExDuration {
-				return diceerrors.NewErrExpireTime("SET")
-			}
-
-			// converting seconds to milliseconds
-			if arg == Ex {
-				exDuration *= 1000
-			}
-			exDurationMs = exDuration
-			state = Initialized
-
-		case Pxat, Exat:
-			if state != Uninitialized {
-				return diceerrors.NewErrWithMessage(diceerrors.SyntaxErr)
-			}
-			i++
-			if i == len(args) {
-				return diceerrors.NewErrWithMessage(diceerrors.SyntaxErr)
-			}
-			exDuration, err := strconv.ParseInt(args[i], 10, 64)
-			if err != nil {
-				return diceerrors.NewErrWithMessage(diceerrors.IntOrOutOfRangeErr)
-			}
-
-			if exDuration < 0 {
-				return diceerrors.NewErrExpireTime("SET")
-			}
-
-			if arg == Exat {
-				exDuration *= 1000
-			}
-			exDurationMs = exDuration - utils.GetCurrentTime().UnixMilli()
-			// If the expiry time is in the past, set exDurationMs to 0
-			// This will be used to signal immediate expiration
-			if exDurationMs < 0 {
-				exDurationMs = 0
-			}
-			state = Initialized
-
-		case XX:
-			// Get the key from the hash table
-			obj := store.Get(key)
-
-			// if key does not exist, return RESP encoded nil
-			if obj == nil {
-				return clientio.RespNIL
-			}
-		case NX:
-			obj := store.Get(key)
-			if obj != nil {
-				return clientio.RespNIL
-			}
-		case KEEPTTL, Keepttl:
-			keepttl = true
-		default:
-			return diceerrors.NewErrWithMessage(diceerrors.SyntaxErr)
-		}
-	}
-
-	// Cast the value properly based on the encoding type
-	var storedValue interface{}
-	switch oEnc {
-	case object.ObjEncodingInt:
-		storedValue, _ = strconv.ParseInt(value, 10, 64)
-	case object.ObjEncodingEmbStr, object.ObjEncodingRaw:
-		storedValue = value
-	default:
-		return clientio.Encode(fmt.Errorf("ERR unsupported encoding: %d", oEnc), false)
-	}
-
-	// putting the k and value in a Hash Table
-	store.Put(key, store.NewObj(storedValue, exDurationMs, oType, oEnc), dstore.WithKeepTTL(keepttl))
-
 	return clientio.RespOK
 }
 
@@ -268,52 +132,6 @@ func evalMSET(args []string, store *dstore.Store) []byte {
 
 	store.PutAll(insertMap)
 	return clientio.RespOK
-}
-
-// evalGET returns the value for the queried key in args
-// The key should be the only param in args
-// The RESP value of the key is encoded and then returned
-// evalGET returns response.RespNIL if key is expired or it does not exist
-func evalGET(args []string, store *dstore.Store) []byte {
-	if len(args) != 1 {
-		return diceerrors.NewErrArity("GET")
-	}
-
-	key := args[0]
-
-	obj := store.Get(key)
-
-	// if key does not exist, return RESP encoded nil
-	if obj == nil {
-		return clientio.RespNIL
-	}
-
-	// Decode and return the value based on its encoding
-	switch _, oEnc := object.ExtractTypeEncoding(obj); oEnc {
-	case object.ObjEncodingInt:
-		// Value is stored as an int64, so use type assertion
-		if val, ok := obj.Value.(int64); ok {
-			return clientio.Encode(val, false)
-		}
-		return diceerrors.NewErrWithFormattedMessage("expected int64 but got another type: %s", obj.Value)
-
-	case object.ObjEncodingEmbStr, object.ObjEncodingRaw:
-		// Value is stored as a string, use type assertion
-		if val, ok := obj.Value.(string); ok {
-			return clientio.Encode(val, false)
-		}
-		return diceerrors.NewErrWithMessage("expected string but got another type")
-
-	case object.ObjEncodingByteArray:
-		// Value is stored as a bytearray, use type assertion
-		if val, ok := obj.Value.(*ByteArray); ok {
-			return clientio.Encode(string(val.data), false)
-		}
-		return diceerrors.NewErrWithMessage(diceerrors.WrongTypeErr)
-
-	default:
-		return diceerrors.NewErrWithMessage(diceerrors.WrongTypeErr)
-	}
 }
 
 // evalDBSIZE returns the number of keys in the database.
@@ -3048,36 +2866,6 @@ func evalLLEN(args []string, store *dstore.Store) []byte {
 
 	deq := obj.Value.(*Deque)
 	return clientio.Encode(deq.Length, false)
-}
-
-// GETSET atomically sets key to value and returns the old value stored at key.
-// Returns an error when key exists but does not hold a string value.
-// Any previous time to live associated with the key is
-// discarded on successful SET operation.
-//
-// Returns:
-// Bulk string reply: the old value stored at the key.
-// Nil reply: if the key does not exist.
-func evalGETSET(args []string, store *dstore.Store) []byte {
-	if len(args) != 2 {
-		return diceerrors.NewErrArity("GETSET")
-	}
-
-	key, value := args[0], args[1]
-	getResp := evalGET([]string{key}, store)
-	// Check if it's an error resp from GET
-	if strings.HasPrefix(string(getResp), "-") {
-		return getResp
-	}
-
-	// Previous TTL needs to be reset
-	setResp := evalSET([]string{key, value}, store)
-	// Check if it's an error resp from SET
-	if strings.HasPrefix(string(setResp), "-") {
-		return setResp
-	}
-
-	return getResp
 }
 
 func evalFLUSHDB(args []string, store *dstore.Store) []byte {
