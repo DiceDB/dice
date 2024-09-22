cee0d30980897d039be463264e09cdec117ee506
H <hash.one.f@gmail.com>
2024-09-22 17:13:23 +0530

    Add integration tests for INCRBYFLOAT

---
+++ integration_tests/commands/incr_by_float_test.go
@@ -0,0 +1,106 @@
+package commands
+
+import (
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestINCRBYFLOAT(t *testing.T) {
+	conn := getLocalConnection()
+	defer conn.Close()
+	invalidArgMessage := "ERR wrong number of arguments for 'incrbyfloat' command"
+	invalidValueTypeMessage := "WRONGTYPE Operation against a key holding the wrong kind of value"
+	invalidIncrTypeMessage := "ERR value is not an integer or a float"
+	valueOutOfRangeMessage := "ERR value is out of range"
+
+	testCases := []struct {
+		name      string
+		setupData string
+		commands  []string
+		expected  []interface{}
+		cleanUp   []string
+	}{
+		{
+			name:      "Invalid number of arguments",
+			setupData: "",
+			commands:  []string{"INCRBYFLOAT", "INCRBYFLOAT foo"},
+			expected:  []interface{}{invalidArgMessage, invalidArgMessage},
+			cleanUp:   []string{},
+		},
+		{
+			name:      "Increment a non existing key",
+			setupData: "",
+			commands:  []string{"INCRBYFLOAT foo 0.1", "GET foo"},
+			expected:  []interface{}{"0.1", "0.1"},
+			cleanUp:   []string{},
+		},
+		{
+			name:      "Increment a key with an integer value",
+			setupData: "SET foo 1",
+			commands:  []string{"INCRBYFLOAT foo 0.1", "GET foo"},
+			expected:  []interface{}{"1.1", "1.1"},
+			cleanUp:   []string{},
+		},
+		{
+			name:      "Increment and then decrement a key with the same value",
+			setupData: "SET foo 1",
+			commands:  []string{"INCRBYFLOAT foo 0.1", "GET foo", "INCRBYFLOAT foo -0.1", "GET foo"},
+			expected:  []interface{}{"1.1", "1.1", "1", "1"},
+			cleanUp:   []string{},
+		},
+		{
+			name:      "Increment a non numeric value",
+			setupData: "SET foo bar",
+			commands:  []string{"INCRBYFLOAT foo 0.1"},
+			expected:  []interface{}{invalidValueTypeMessage},
+			cleanUp:   []string{},
+		},
+		{
+			name:      "Increment by a non numeric value",
+			setupData: "SET foo 1",
+			commands:  []string{"INCRBYFLOAT foo bar"},
+			expected:  []interface{}{invalidIncrTypeMessage},
+			cleanUp:   []string{},
+		},
+		{
+			name:      "Increment by both integer and float",
+			setupData: "SET foo 1",
+			commands:  []string{"INCRBYFLOAT foo 1", "INCRBYFLOAT foo 0.1"},
+			expected:  []interface{}{"2", "2.1"},
+			cleanUp:   []string{},
+		},
+		{
+			name:      "Increment that would make the value Inf",
+			setupData: "SET foo 1e308",
+			commands:  []string{"INCRBYFLOAT foo 1e308", "INCRBYFLOAT foo -1e308"},
+			expected:  []interface{}{valueOutOfRangeMessage, "0"},
+			cleanUp:   []string{},
+		},
+		{
+			name:      "Increment that would make the value -Inf",
+			setupData: "SET foo -1e308",
+			commands:  []string{"INCRBYFLOAT foo -1e308", "INCRBYFLOAT foo 1e308"},
+			expected:  []interface{}{valueOutOfRangeMessage, "0"},
+			cleanUp:   []string{},
+		},
+	}
+
+	for _, tc := range testCases {
+		FireCommand(conn, "DEL foo")
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.setupData != "" {
+				assert.Equal(t, FireCommand(conn, tc.setupData), "OK")
+			}
+			for i := 0; i < len(tc.commands); i++ {
+				cmd := tc.commands[i]
+				out := tc.expected[i]
+				result := FireCommand(conn, cmd)
+				assert.Equal(t, out, result)
+			}
+			for i := 0; i < len(tc.cleanUp); i++ {
+				FireCommand(conn, tc.cleanUp[i])
+			}
+		})
+	}
+}
