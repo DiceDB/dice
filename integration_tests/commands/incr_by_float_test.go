29c03b704e8f21541c4fbc1b394511f54d5030bd
H <hash.one.f@gmail.com>
2024-09-22 17:24:48 +0530

    Remove cleanup field

--- integration_tests/commands/incr_by_float_test.go
+++ integration_tests/commands/incr_by_float_test.go
@@ -19,70 +19,60 @@ func TestINCRBYFLOAT(t *testing.T) {
 		setupData string
 		commands  []string
 		expected  []interface{}
-		cleanUp   []string
 	}{
 		{
 			name:      "Invalid number of arguments",
 			setupData: "",
 			commands:  []string{"INCRBYFLOAT", "INCRBYFLOAT foo"},
 			expected:  []interface{}{invalidArgMessage, invalidArgMessage},
-			cleanUp:   []string{},
 		},
 		{
 			name:      "Increment a non existing key",
 			setupData: "",
 			commands:  []string{"INCRBYFLOAT foo 0.1", "GET foo"},
 			expected:  []interface{}{"0.1", "0.1"},
-			cleanUp:   []string{},
 		},
 		{
 			name:      "Increment a key with an integer value",
 			setupData: "SET foo 1",
 			commands:  []string{"INCRBYFLOAT foo 0.1", "GET foo"},
 			expected:  []interface{}{"1.1", "1.1"},
-			cleanUp:   []string{},
 		},
 		{
 			name:      "Increment and then decrement a key with the same value",
 			setupData: "SET foo 1",
 			commands:  []string{"INCRBYFLOAT foo 0.1", "GET foo", "INCRBYFLOAT foo -0.1", "GET foo"},
 			expected:  []interface{}{"1.1", "1.1", "1", "1"},
-			cleanUp:   []string{},
 		},
 		{
 			name:      "Increment a non numeric value",
 			setupData: "SET foo bar",
 			commands:  []string{"INCRBYFLOAT foo 0.1"},
 			expected:  []interface{}{invalidValueTypeMessage},
-			cleanUp:   []string{},
 		},
 		{
 			name:      "Increment by a non numeric value",
 			setupData: "SET foo 1",
 			commands:  []string{"INCRBYFLOAT foo bar"},
 			expected:  []interface{}{invalidIncrTypeMessage},
-			cleanUp:   []string{},
 		},
 		{
 			name:      "Increment by both integer and float",
 			setupData: "SET foo 1",
 			commands:  []string{"INCRBYFLOAT foo 1", "INCRBYFLOAT foo 0.1"},
 			expected:  []interface{}{"2", "2.1"},
-			cleanUp:   []string{},
 		},
 		{
 			name:      "Increment that would make the value Inf",
 			setupData: "SET foo 1e308",
 			commands:  []string{"INCRBYFLOAT foo 1e308", "INCRBYFLOAT foo -1e308"},
 			expected:  []interface{}{valueOutOfRangeMessage, "0"},
-			cleanUp:   []string{},
 		},
 		{
 			name:      "Increment that would make the value -Inf",
 			setupData: "SET foo -1e308",
 			commands:  []string{"INCRBYFLOAT foo -1e308", "INCRBYFLOAT foo 1e308"},
 			expected:  []interface{}{valueOutOfRangeMessage, "0"},
-			cleanUp:   []string{},
 		},
 	}
 
@@ -97,9 +87,6 @@ func TestINCRBYFLOAT(t *testing.T) {
 				out := tc.expected[i]
 				result := FireCommand(conn, cmd)
 				assert.Equal(t, out, result)
-			}
-			for i := 0; i < len(tc.cleanUp); i++ {
-				FireCommand(conn, tc.cleanUp[i])
 			}
 		})
 	}
