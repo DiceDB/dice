f512096d7fd8b79dc9243086c9d6d3405a04f8f8
KaviiSuri <surikavii@gmail.com>
2024-09-26 23:37:30 +0530

    Fix Child Process not working for BGREWRITEAOF on MacOS + Logging Improvements (#704)

--- internal/logger/zerolog.go
+++ internal/logger/zerolog.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"context"
 	"log/slog"
+	"time"
 
 	"github.com/rs/zerolog"
 )
@@ -24,31 +25,83 @@ func newZerologHandler(logger *zerolog.Logger) *ZerologHandler {
 //nolint:gocritic // The slog.Record struct triggers hugeParam but we don't control the interface (it's a standard library one)
 func (h *ZerologHandler) Handle(ctx context.Context, record slog.Record) error {
 	event := h.logger.WithLevel(mapLevel(record.Level))
-
 	record.Attrs(func(attr slog.Attr) bool {
-		switch attr.Value.Kind() {
-		case slog.KindString:
-			event = event.Str(attr.Key, attr.Value.String())
-		case slog.KindInt64:
-			event = event.Int64(attr.Key, attr.Value.Int64())
-		case slog.KindFloat64:
-			event = event.Float64(attr.Key, attr.Value.Float64())
-		case slog.KindBool:
-			event = event.Bool(attr.Key, attr.Value.Bool())
-		default:
-			switch v := attr.Value.Any().(type) {
-			// error is a special case since zerlog has a dedicated method for it but it's not part of the slog.Kind
-			case error:
-				event = event.Err(v)
-			default:
-				event = event.Interface(attr.Key, attr.Value.Any())
-			}
-		}
+		addAttrToZerolog(attr, event)
 		return true
 	})
-
 	event.Msg(record.Message)
 	return nil
+}
+
+// Enabled implements the slog.Handler interface
+func (h *ZerologHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.logger.GetLevel() <= mapLevel(level)
+}
+
+// WithAttrs adds attributes to the log event
+func (h *ZerologHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	ctx := h.logger.With()
+	for _, attr := range attrs {
+		ctx = addAttrToZerolog(attr, ctx)
+	}
+	logger := ctx.Logger()
+	return newZerologHandler(&logger)
+}
+
+// WithGroup returns the handler for a group of logs
+func (h *ZerologHandler) WithGroup(name string) slog.Handler {
+	logger := h.logger.With().Str("group", name).Logger()
+	return newZerologHandler(&logger)
+}
+
+// addAttrToZerolog is a generic function to add an slog.Attr to either a zerolog.Event or zerolog.Context
+func addAttrToZerolog[T interface {
+	Str(string, string) T
+	Int64(string, int64) T
+	Uint64(string, uint64) T
+	Float64(string, float64) T
+	Bool(string, bool) T
+	Err(error) T
+	Time(string, time.Time) T
+	Dur(string, time.Duration) T
+	Interface(string, any) T
+	AnErr(key string, err error) T
+}](attr slog.Attr, target T) T {
+	switch attr.Value.Kind() {
+	case slog.KindBool:
+		return target.Bool(attr.Key, attr.Value.Bool())
+	case slog.KindDuration:
+		return target.Dur(attr.Key, attr.Value.Duration())
+	case slog.KindFloat64:
+		return target.Float64(attr.Key, attr.Value.Float64())
+	case slog.KindInt64:
+		return target.Int64(attr.Key, attr.Value.Int64())
+	case slog.KindString:
+		return target.Str(attr.Key, attr.Value.String())
+	case slog.KindTime:
+		return target.Time(attr.Key, attr.Value.Time())
+	case slog.KindUint64:
+		return target.Uint64(attr.Key, attr.Value.Uint64())
+	case slog.KindGroup:
+		// For group, we need to recurse into the group's attributes
+		group := attr.Value.Group()
+		for _, groupAttr := range group {
+			target = addAttrToZerolog(groupAttr, target)
+		}
+		return target
+	case slog.KindLogValuer:
+		// LogValuer is a special case that needs to be resolved
+		resolved := attr.Value.Resolve()
+		return addAttrToZerolog(slog.Attr{Key: attr.Key, Value: resolved}, target)
+	default:
+		switch v := attr.Value.Any().(type) {
+		// error is a special case since zerlog has a dedicated method for it but it's not part of the slog.Kind
+		case error:
+			return target.AnErr(attr.Key, v)
+		default:
+			return target.Interface(attr.Key, attr.Value)
+		}
+	}
 }
 
 // mapLevel maps slog levels to zerolog levels
@@ -63,24 +116,4 @@ func mapLevel(level slog.Level) zerolog.Level {
 	default:
 		return zerolog.InfoLevel
 	}
-}
-
-// Enabled implements the slog.Handler interface
-func (h *ZerologHandler) Enabled(ctx context.Context, level slog.Level) bool {
-	return h.logger.GetLevel() <= mapLevel(level)
-}
-
-// WithAttrs adds attributes to the log event
-func (h *ZerologHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	logger := *h.logger
-	for _, attr := range attrs {
-		logger = logger.With().Interface(attr.Key, attr.Value).Logger()
-	}
-	return newZerologHandler(&logger)
-}
-
-// WithGroup returns the handler for a group of logs
-func (h *ZerologHandler) WithGroup(name string) slog.Handler {
-	logger := h.logger.With().Str("group", name).Logger()
-	return newZerologHandler(&logger)
 }
