284646aee5f2c8bf2a50e19376e7c639c697bb47
Aditya <adityab7968@gmail.com>
2024-09-30 21:41:40 +0530

    removed fuzz/stress test

--- integration_tests/commands/async/append_test.go
+++ integration_tests/commands/async/append_test.go
@@ -1,31 +1,14 @@
 package async
 
 import (
-	"math/rand"
 	"testing"
-	"time"
 
 	"gotest.tools/v3/assert"
 )
 
-func randString(n int, charset string) string {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	b := make([]byte, n)
-	for i := range b {
-		b[i] = charset[r.Intn(len(charset))]
-	}
-	return string(b)
-}
-
 func TestAPPEND(t *testing.T) {
 	conn := getLocalConnection()
 	defer conn.Close()
-
-	types := map[string]string{
-		"binary": "01",
-		"alpha":  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-		"compr":  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-	}
 
 	testCases := []struct {
 		name     string
@@ -59,32 +42,6 @@ func TestAPPEND(t *testing.T) {
 			},
 			expected: []interface{}{int64(0), int64(1), int64(2), "12", "OK", int64(2), "12"},
 		},
-	}
-
-	for fuzzType, charset := range types {
-		buf := ""
-		commands := []string{"DEL key"}
-		expected := []interface{}{int64(0)}
-
-		for i := 0; i < 1000; i++ {
-			bin := randString(rand.Intn(10)+1, charset)
-			buf += bin
-			commands = append(commands, "APPEND key "+bin)
-			expected = append(expected, int64(len(buf)))
-		}
-
-		commands = append(commands, "GET key")
-		expected = append(expected, buf)
-
-		testCases = append(testCases, struct {
-			name     string
-			commands []string
-			expected []interface{}
-		}{
-			name:     "APPEND Fuzzing with " + fuzzType,
-			commands: commands,
-			expected: expected,
-		})
 	}
 
 	for _, tc := range testCases {
