82b96c564ed0f25a7015de652f3170a3ee10221e
Jyotinder Singh <jyotindrsingh@gmail.com>
2024-10-04 13:16:24 +0530

    Update integration_tests/commands/async/bitfield_test.go

--- integration_tests/commands/async/bitfield_test.go
+++ integration_tests/commands/async/bitfield_test.go
@@ -11,6 +11,7 @@ func TestBitfield(t *testing.T) {
 	conn := getLocalConnection()
 	defer conn.Close()
 
+        FireCommand(conn, "FLUSHDB")
 	defer FireCommand(conn, "FLUSHDB") // clean up after all test cases
 	syntaxErrMsg := "ERR syntax error"
 	bitFieldTypeErrMsg := "ERR Invalid bitfield type. Use something like i16 u8. Note that u64 is not supported but i64 is."
