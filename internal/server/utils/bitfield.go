9dc179b88697eed91500955b33fc82f45506c2cb
Saubhik Kumar <saubhik.pandey@gmail.com>
2024-10-08 19:48:07 +0530

    add name to return type hints

--- internal/server/utils/bitfield.go
+++ internal/server/utils/bitfield.go
@@ -67,9 +67,8 @@ func parseEncodingAndOffset(args []string) (eType, eVal, offset interface{}, err
 }
 
 
-func ParseOps(args []string, readOnly bool) ([]BitFieldOp, []byte) {
+func ParseOps(args []string, readOnly bool) (ops []BitFieldOp, err []byte) {
 	overflowType := WRAP // Default overflow type
-	var ops []BitFieldOp
 
 	for i := 1; i < len(args); {
 		isReadOnlyCommand := false
