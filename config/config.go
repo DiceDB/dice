7de5908ccd7e511e764113d1682a2c62530389d6
RohanChavan <rohanchavan1918@gmail.com>
2024-10-08 13:54:30 +0530

    Get adhocReqChan buffer size from config

--- config/config.go
+++ config/config.go
@@ -72,6 +72,7 @@ type Config struct {
 		EnableMultiThreading   bool          `mapstructure:"enablemultithreading"`
 		StoreMapInitSize       int           `mapstructure:"storemapinitsize"`
 		WatchChanBufSize       int           `mapstructure:"watchchanbufsize"`
+		AdhocReqChanBufSize    int           `mapstructure:"adhocreqchanbufsize"`
 	} `mapstructure:"server"`
 	Auth struct {
 		UserName string `mapstructure:"username"`
@@ -106,6 +107,7 @@ var baseConfig = Config{
 		EnableMultiThreading   bool          `mapstructure:"enablemultithreading"`
 		StoreMapInitSize       int           `mapstructure:"storemapinitsize"`
 		WatchChanBufSize       int           `mapstructure:"watchchanbufsize"`
+		AdhocReqChanBufSize    int           `mapstructure:"adhocreqchanbufsize"`
 	}{
 		Addr:                   DefaultHost,
 		Port:                   DefaultPort,
@@ -127,6 +129,7 @@ var baseConfig = Config{
 		EnableMultiThreading:   false,
 		StoreMapInitSize:       1024000,
 		WatchChanBufSize:       20000,
+		AdhocReqChanBufSize:    20,
 	},
 	Auth: struct {
 		UserName string `mapstructure:"username"`
