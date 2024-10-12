4dcebaa91aa1efe0b44249491b1ae38eb7bb441b
Apoorv Yadav <32174554+apoorvyadav1111@users.noreply.github.com>
2024-10-11 23:17:55 -0400

    fixes #1070 (#1071)

--- internal/server/server.go
+++ internal/server/server.go
@@ -320,6 +320,7 @@ func (s *AsyncServer) executeCommandToBuffer(diceDBCmd *cmd.DiceDBCmd, buf *byte
 		// Handle error case independently
 		if resp.EvalResponse.Error != nil {
 			handleMigratedResp(resp.EvalResponse.Error, buf)
+			return
 		}
 		handleMigratedResp(resp.EvalResponse.Result, buf)
 		return
