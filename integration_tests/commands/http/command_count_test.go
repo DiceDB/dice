b339233260e10457ff5cabc226ad17eb2f289ade
svkaizoku <svignesh81235@gmail.com>
2024-10-14 20:00:48 +0200

    Fix merge

--- integration_tests/commands/http/command_count_test.go
+++ integration_tests/commands/http/command_count_test.go
@@ -9,6 +9,31 @@ import (
 
 func TestCommandCount(t *testing.T) {
 	exec := NewHTTPCommandExecutor()
+
+	testCases := []TestCase{
+		{
+			name: "Command count should not support any argument",
+			commands: []HTTPCommand{
+				{Command: "COMMAND/COUNT", Body: map[string]interface{}{"key": ""}},
+			},
+			expected: []interface{}{"ERR wrong number of arguments for 'command|count' command"},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			for _, cmd := range tc.commands {
+				result, _ := exec.FireCommand(cmd)
+				assert.DeepEqual(t, tc.expected[i], result)
+
+			}
+
+		})
+	}
+}
+
+func TestCommandCount1(t *testing.T) {
+	exec := NewHTTPCommandExecutor()
 	t.Run("Command count should be positive", func(t *testing.T) {
 		commandCount := getCommandCount(exec)
 		assert.Assert(t, commandCount > 0,
