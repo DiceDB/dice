3ca4c51ba1d2670c9a8ea5135c2f95b508365ef8
psr <prateek.singh.rathore@gmail.com>
2024-10-14 17:07:26 +0530

    updated parsing logic to support qwatch and qunwatch commands

--- internal/server/utils/redisCmdAdapter.go
+++ internal/server/utils/redisCmdAdapter.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/dicedb/dice/internal/cmd"
@@ -32,6 +33,9 @@ const (
 	Index       = "index"
 	JSON        = "json"
 )
+
+const QWatch string = "QWATCH"
+const QUnwatch string = "QUNWATCH"
 
 func ParseHTTPRequest(r *http.Request) (*cmd.DiceDBCmd, error) {
 	commandParts := strings.Split(strings.TrimPrefix(r.URL.Path, "/"), "/")
@@ -117,16 +121,38 @@ func ParseHTTPRequest(r *http.Request) (*cmd.DiceDBCmd, error) {
 }
 
 func ParseWebsocketMessage(msg []byte) (*cmd.DiceDBCmd, error) {
-	cmdStr := string(msg)
-	cmdStr = strings.TrimSpace(cmdStr)
-
+	cmdStr := strings.TrimSpace(string(msg))
 	if cmdStr == "" {
 		return nil, diceerrors.ErrEmptyCommand
 	}
 
-	cmdArr := strings.Split(cmdStr, " ")
-	command := strings.ToUpper(cmdArr[0])
-	cmdArr = cmdArr[1:] // args
+	var command string
+	idx := strings.Index(cmdStr, " ")
+	// handle commands with no args
+	if idx == -1 {
+		command = strings.ToUpper(cmdStr)
+		return &cmd.DiceDBCmd{
+			Cmd:  command,
+			Args: nil,
+		}, nil
+	} else {
+		command = strings.ToUpper(cmdStr[:idx])
+		cmdStr = cmdStr[idx+1:]
+	}
+
+	cmdArr := []string{} // args
+	// handle qwatch and qunwatch commands
+	if command == QWatch || command == QUnwatch {
+		// remove quotes from query string
+		if cmdStr, err := strconv.Unquote(cmdStr); err != nil {
+			return nil, fmt.Errorf("error parsing qwatch query: %v", err)
+		} else {
+			cmdArr = []string{cmdStr}
+		}
+	} else {
+		// handle other commands
+		cmdArr = strings.Split(cmdStr, " ")
+	}
 
 	// if key prefix is empty for JSON.INGEST command
 	// add "" to cmdArr
