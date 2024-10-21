aacbecb1a7ecdffa7dce908643984f9e57d666f7
psr <prateek.singh.rathore@gmail.com>
2024-10-21 11:13:44 +0530

    a

--- internal/server/websocketServer.go
+++ internal/server/websocketServer.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/dicedb/dice/internal/server/abstractserver"
 	"log/slog"
 	"net"
 	"net/http"
@@ -14,6 +13,8 @@ import (
 	"sync"
 	"syscall"
 	"time"
+
+	"github.com/dicedb/dice/internal/server/abstractserver"
 
 	"github.com/dicedb/dice/config"
 	"github.com/dicedb/dice/internal/clientio"
