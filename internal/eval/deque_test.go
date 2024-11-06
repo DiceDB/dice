3ef322e88c85f7f06dbe06387e239152f0fdcf0a
Sah Oss <weloveunited56@gmail.com>
2024-11-06 07:38:34 +0530

    fix

--- internal/eval/deque_test.go
+++ internal/eval/deque_test.go
@@ -2,6 +2,7 @@ package eval_test
 
 import (
 	"fmt"
+	"gotest.tools/v3/assert"
 	"math/rand"
 	"strconv"
 	"strings"
@@ -9,7 +10,6 @@ import (
 	"time"
 
 	"github.com/dicedb/dice/internal/eval"
-	"github.com/stretchr/testify/assert"
 )
 
 var deqRandGenerator *rand.Rand
