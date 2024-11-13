c9ceaa7779fbeeb9a54b88e3b65a48abfa6fa213
Kaushik Kakdey <kaushik.kakdey@gmail.com>
2024-11-13 23:16:17 +0530

    supports new Eval struct impl

--- internal/eval/execute.go
+++ internal/eval/execute.go
@@ -53,7 +53,7 @@ func (e *Eval) ExecuteCommand() *EvalResponse {
 		// ===============================================================================
 		// dealing with store object is not recommended for all commands
 		// These operations are specialised for the commands which requires
-		// transfering data across multiple shards. e.g COPY, RENAME
+		// transferring data across multiple shards. e.g COPY, RENAME
 		// ===============================================================================
 		if e.cmd.InternalObj != nil {
 			// This involves handling object at store level, evaluating it, modifying it, and then storing it back.
@@ -72,7 +72,7 @@ func (e *Eval) ExecuteCommand() *EvalResponse {
 	// to the new implementation soon for all commands
 
 	case "CLIENT":
-		return &EvalResponse{Result: EvalCLIENT(c.Args, httpOp, client, store), Error: nil}
+		return &EvalResponse{Result: EvalCLIENT(e.cmd.Args, e.isHTTPOperation, e.client, e.store), Error: nil}
 	case "SUBSCRIBE", "Q.WATCH":
 		return &EvalResponse{Result: EvalQWATCH(e.cmd.Args, e.isHTTPOperation, e.isWebSocketOperation, e.client, e.store), Error: nil}
 	case "UNSUBSCRIBE", "Q.UNWATCH":
