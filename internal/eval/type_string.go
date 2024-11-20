0f1272a4e83e4cc4983840847e4ddf2b94a6af0d
Dhanraj Prem Kotian <dhanrajkotian3@gmail.com>
2024-11-20 16:25:21 -0500

    #1036 Preserve TTL after APPEND command (#1037)

--- internal/eval/type_string.go
+++ internal/eval/type_string.go
@@ -3,17 +3,73 @@ package eval
 import (
 	"strconv"
 
-	dstore "github.com/dicedb/dice/internal/object"
+	diceerrors "github.com/dicedb/dice/internal/errors"
+	"github.com/dicedb/dice/internal/object"
 )
 
 // Similar to
 // tryObjectEncoding function in Redis
 func deduceTypeEncoding(v string) (o, e uint8) {
+	// Check if the value has leading zero
+	if len(v) > 1 && v[0] == '0' {
+		// If so, treat as string
+		return object.ObjTypeString, object.ObjEncodingRaw
+	}
 	if _, err := strconv.ParseInt(v, 10, 64); err == nil {
-		return dstore.ObjTypeInt, dstore.ObjEncodingInt
+		return object.ObjTypeInt, object.ObjEncodingInt
 	}
 	if len(v) <= 44 {
-		return dstore.ObjTypeString, dstore.ObjEncodingEmbStr
+		return object.ObjTypeString, object.ObjEncodingEmbStr
 	}
-	return dstore.ObjTypeString, dstore.ObjEncodingRaw
+	return object.ObjTypeString, object.ObjEncodingRaw
+}
+
+// Function to handle converting the value based on the encoding type
+func storeValueWithEncoding(value string, oEnc uint8) (interface{}, error) {
+	var returnValue interface{}
+
+	// treat as string if value has leading zero
+	if len(value) > 1 && value[0] == '0' {
+		// If so, treat as string
+		return value, nil
+	}
+
+	switch oEnc {
+	case object.ObjEncodingInt:
+		intValue, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return nil, diceerrors.ErrWrongTypeOperation
+		}
+		returnValue = intValue
+	case object.ObjEncodingEmbStr, object.ObjEncodingRaw:
+		returnValue = value
+	default:
+		return nil, diceerrors.ErrWrongTypeOperation
+	}
+
+	return returnValue, nil
+}
+
+// Function to convert the value to a string for concatenation or manipulation
+func convertValueToString(obj *object.Obj, oEnc uint8) (string, error) {
+	var currentValueStr string
+
+	switch oEnc {
+	case object.ObjEncodingInt:
+		// Convert int64 to string for concatenation
+		currentValueStr = strconv.FormatInt(obj.Value.(int64), 10)
+	case object.ObjEncodingEmbStr, object.ObjEncodingRaw:
+		// Use the string value directly
+		currentValueStr = obj.Value.(string)
+	case object.ObjEncodingByteArray:
+		val, ok := obj.Value.(*ByteArray)
+		if !ok {
+			return "", diceerrors.ErrWrongTypeOperation
+		}
+		currentValueStr = string(val.data)
+	default:
+		return "", diceerrors.ErrWrongTypeOperation
+	}
+
+	return currentValueStr, nil
 }
