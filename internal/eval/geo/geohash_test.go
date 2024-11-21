d913b6e39bc808af9b901e53a357420894861629
tdadadavid <davidtofunmidada@gmail.com>
2024-11-21 14:24:30 +0100

    add test for invalid inputs

--- internal/eval/geo/geohash_test.go
+++ internal/eval/geo/geohash_test.go
@@ -5,14 +5,35 @@ import "testing"
 
 
 func TestGeoHash_Encode(t *testing.T)  {
-	
-	t.Run("Return error when invalid longitude and latitude range is given", func ()  {
-		
-	})
+	tests := []struct{
+		name      string
+		long      float64
+		lat       float64
+		precision int
+		want      string
+		wantErr   bool
+	}{
+		// Invalid cases
+		{"Longitude Out of Range (high)", 200.0, 0.0, 5, "", true},
+		{"Longitude Out of Range (low)", -200.0, 0.0, 5, "", true},
+		{"Latitude Out of Range (high)", 0.0, 100.0, 5, "", true},
+		{"Latitude Out of Range (low)", 0.0, -100.0, 5, "", true},
+		{"Precision Too Low", 0.0, 0.0, 0, "", true},
+		{"Precision Too High", 0.0, 0.0, 33, "", true},
+	}
 
-	t.Run("Return error when invalid longitude and latitude range is given", func ()  {
-		
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := EncodeGeoHash(tt.long, tt.lat, tt.precision)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("EncodeGeoHash() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("EncodeGeoHash() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
 func TestGeoHash_Decode(t *testing.T) {
