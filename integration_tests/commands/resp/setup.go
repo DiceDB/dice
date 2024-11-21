eac3ce83dfa41342150099899a26b73beb1cd1d9
apoorvyadav1111 <apoorvyadav1111@gmail.com>
2024-11-21 09:56:45 -0500

    Revert "fixed linter"
    This reverts commit ff6b10675e8e6e178651a507c18591612745af75.

--- integration_tests/commands/resp/setup.go
+++ integration_tests/commands/resp/setup.go
@@ -10,14 +10,12 @@ import (
 	"net"
 	"os"
 	"sync"
-	"testing"
 	"time"
 
 	"github.com/dicedb/dice/internal/server/resp"
 	"github.com/dicedb/dice/internal/wal"
 	"github.com/dicedb/dice/internal/watchmanager"
 	"github.com/dicedb/dice/internal/worker"
-	"github.com/stretchr/testify/assert"
 
 	"github.com/dicedb/dice/config"
 	"github.com/dicedb/dice/internal/clientio"
@@ -61,30 +59,6 @@ func ClosePublisherSubscribers(publisher net.Conn, subscribers []net.Conn) error
 		}
 	}
 	return nil
-}
-
-//nolint:unused
-func unsubscribeFromWatchUpdates(t *testing.T, subscribers []net.Conn, cmd, fingerprint string) {
-	t.Helper()
-	for _, subscriber := range subscribers {
-		rp := fireCommandAndGetRESPParser(subscriber, fmt.Sprintf("%s.UNWATCH %s", cmd, fingerprint))
-		assert.NotNil(t, rp)
-		v, err := rp.DecodeOne()
-		assert.NoError(t, err)
-		castedValue, ok := v.(string)
-		if !ok {
-			t.Errorf("Type assertion to string failed for value: %v", v)
-		}
-		assert.Equal(t, castedValue, "OK")
-	}
-}
-
-//nolint:unused
-func unsubscribeFromWatchUpdatesSDK(t *testing.T, subscribers []WatchSubscriber, cmd, fingerprint string) {
-	for _, subscriber := range subscribers {
-		err := subscriber.watch.Unwatch(context.Background(), cmd, fingerprint)
-		assert.Nil(t, err)
-	}
 }
 
 // deleteTestKeys is a utility to delete a list of keys before running a test
