c38897d813ad6d65e91843447f6d6fc0d70895e1
Saubhik Kumar <saubhik.pandey@gmail.com>
2024-12-08 18:38:46 +0530

    add new method for permutation match

--- integration_tests/commands/websocket/json_test.go
+++ integration_tests/commands/websocket/json_test.go
@@ -59,7 +59,7 @@ func runIntegrationTests(t *testing.T, exec *WebsocketCommandExecutor, conn *web
 				case "json_equal":
 					assert.JSONEq(t, out.(string), result.(string))
 				case "deep_equal":
-					assert.True(t, testutils.ArraysArePermutations(result.([]interface{}), out.([]interface{})))
+					assert.ElementsMatch(t, result.([]interface{}), out.([]interface{}))
 				}
 			}
 		})
