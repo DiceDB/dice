bb75efdf5965afc229c15f9d788b106d34c7fe62
Prashant Shubham <prashant1996cr07@gmail.com>
2024-12-09 15:46:04 +0530

    Disabled multishard cmds and test for HTTP/WS (#1363)

--- internal/iothread/cmd_meta.go
+++ internal/iothread/cmd_meta.go
@@ -129,10 +129,14 @@ const (
 	CmdGetDel              = "GETDEL"
 	CmdLrange              = "LRANGE"
 	CmdLinsert             = "LINSERT"
+	CmdJSONArrInsert       = "JSON.ARRINSERT"
+	CmdJSONArrTrim         = "JSON.ARRTRIM"
 	CmdJSONArrAppend       = "JSON.ARRAPPEND"
 	CmdJSONArrLen          = "JSON.ARRLEN"
 	CmdJSONArrPop          = "JSON.ARRPOP"
 	CmdJSONClear           = "JSON.CLEAR"
+	CmdJSONSet             = "JSON.SET"
+	CmdJSONObjKeys         = "JSON.OBJKEYS"
 	CmdJSONDel             = "JSON.DEL"
 	CmdJSONForget          = "JSON.FORGET"
 	CmdJSONGet             = "JSON.GET"
@@ -267,6 +271,12 @@ var CommandsMeta = map[string]CmdMeta{
 	CmdJSONArrAppend: {
 		CmdType: SingleShard,
 	},
+	CmdJSONArrInsert: {
+		CmdType: SingleShard,
+	},
+	CmdJSONArrTrim: {
+		CmdType: SingleShard,
+	},
 	CmdJSONArrLen: {
 		CmdType: SingleShard,
 	},
@@ -274,6 +284,12 @@ var CommandsMeta = map[string]CmdMeta{
 		CmdType: SingleShard,
 	},
 	CmdJSONClear: {
+		CmdType: SingleShard,
+	},
+	CmdJSONSet: {
+		CmdType: SingleShard,
+	},
+	CmdJSONObjKeys: {
 		CmdType: SingleShard,
 	},
 	CmdJSONDel: {
--- internal/server/httpws/redisCmdAdapter_test.go
+++ internal/server/httpws/redisCmdAdapter_test.go
@@ -1,4 +1,4 @@
-package utils
+package httpws
 
 import (
 	"net/http/httptest"
