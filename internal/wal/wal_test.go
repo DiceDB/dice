9e6531517f7341e57739f12bb47cd3cfc41a3ab2
ayushsatyam146 <ayushsatyam146@gmail.com>
2024-12-12 13:05:22 +0530

    introducing enums and removing pre allocated files

--- internal/wal/wal_test.go
+++ internal/wal/wal_test.go
@@ -8,26 +8,6 @@ import (
 	"github.com/dicedb/dice/internal/wal"
 )
 
-// func BenchmarkLogCommandSQLite(b *testing.B) {
-// 	wl, err := wal.NewSQLiteWAL("/tmp/dicedb-lt")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	if err := wl.Init(time.Now()); err != nil {
-// 		slog.Error("could not initialize WAL", slog.Any("error", err))
-// 	} else {
-// 		go wal.InitBG(wl)
-// 	}
-
-// 	for i := 0; i < b.N; i++ {
-// 		wl.LogCommand(&cmd.DiceDBCmd{
-// 			Cmd:  "SET",
-// 			Args: []string{"key", "value"},
-// 		})
-// 	}
-// }
-
 func BenchmarkLogCommandAOF(b *testing.B) {
 	wl, err := wal.NewAOFWAL("/tmp/dicedb-lt")
 	if err != nil {
