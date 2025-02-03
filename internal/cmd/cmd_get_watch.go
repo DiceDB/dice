d483e9d1d9c2bf0a482d3560c2cff3260fc63d15
Arpit <arpit.b.bhayani@gmail.com>
2025-02-03 11:36:10 +0000

    Fingerprint as additional attrs in .WATCH command

--- internal/cmd/cmd_get_watch.go
+++ internal/cmd/cmd_get_watch.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"strconv"
+
 	dstore "github.com/dicedb/dice/internal/store"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 var cGETWATCH = &DiceDBCommand{
@@ -24,5 +27,12 @@ func evalGETWATCH(c *Cmd, s *dstore.Store) (*CmdRes, error) {
 		return nil, err
 	}
 
+	if r.R.Attrs == nil {
+		r.R.Attrs = &structpb.Struct{
+			Fields: make(map[string]*structpb.Value),
+		}
+	}
+
+	r.R.Attrs.Fields["fingerprint"] = structpb.NewStringValue(strconv.FormatUint(uint64(c.GetFingerprint()), 10))
 	return r, nil
 }
