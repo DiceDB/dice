ed43b5e5c0c2d7911b05ace35ba6078c459a0f62
Pankaj <pankajshadev@gmail.com>
2025-02-14 10:29:40 +0530

    Port  TYPE command (#1475)

---
+++ internal/cmd/cmd_type.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	dstore "github.com/dicedb/dice/internal/store"
+	"github.com/dicedb/dicedb-go/wire"
+)
+
+var cType = &DiceDBCommand{
+	Name:      "TYPE",
+	HelpShort: "Determine data type of the value stored at a specified key",
+	Eval:      evalTYPE,
+}
+
+func init() {
+	commandRegistry.AddCommand(cType)
+}
+
+func evalTYPE(c *Cmd, s *dstore.Store) (*CmdRes, error) {
+	if len(c.C.Args) != 1 {
+		return cmdResNil, errWrongArgumentCount("TYPE")
+	}
+
+	key := c.C.Args[0]
+	obj := s.Get(key)
+
+	if obj == nil {
+		return &CmdRes{R: &wire.Response{
+			Value: &wire.Response_VStr{
+				VStr: "none",
+			},
+		}}, nil
+	}
+
+	typeStr := obj.Type.String()
+
+	return &CmdRes{R: &wire.Response{
+		Value: &wire.Response_VStr{
+			VStr: typeStr,
+		},
+	}}, nil
+}
