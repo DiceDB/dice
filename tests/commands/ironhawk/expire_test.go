bca962bcfdf658a3e8c654ca4acf5a75dd20101d
Hari Om Bhardwaj <hariombhardwaj038@gmail.com>
2025-02-28 11:16:59 +0000

    all test case done except case-sesintive

--- tests/commands/ironhawk/expire_test.go
+++ tests/commands/ironhawk/expire_test.go
@@ -55,11 +55,7 @@ func TestEXPIRE(t *testing.T) {
 				"SET test_key test_value",
 				"EXPIRE test_key",
 			},
-<<<<<<< HEAD
-			expected: []interface{}{"OK", errors.New("wrong number of arguments for 'EXPIRE' command"),},
-=======
 			expected: []interface{}{"OK", errors.New("wrong number of arguments for 'EXPIRE' command")},
->>>>>>> 801af6f (expire tests done)
 		},
 		{
 			name: "Test(NX): Set the expiration only if the key has no expiration time",
@@ -104,13 +100,8 @@ func TestEXPIRE(t *testing.T) {
 		{
 			name: "TEST(LT): Set the expiration only if the new expiration time is less than the current one",
 			commands: []string{
-<<<<<<< HEAD
-				"SET test_key test_value EX 15",
-				"EXPIRE test_key " + strconv.FormatInt(10, 10) + " LT",
-=======
 				"SET test_key test_value",
 				"EXPIRE test_key " + strconv.FormatInt(10, 10),
->>>>>>> 801af6f (expire tests done)
 				"EXPIRE test_key " + strconv.FormatInt(20, 10) + " LT",
 			},
 			expected: []interface{}{"OK", 1, 0},
@@ -118,12 +109,8 @@ func TestEXPIRE(t *testing.T) {
 		{
 			name: "TEST(LT): Set the expiration only if the new expiration time is less than the current one",
 			commands: []string{
-<<<<<<< HEAD
-				"SET test_key test_value EX 15",
-=======
 				"SET test_key test_value",
 				"EXPIRE test_key " + strconv.FormatInt(20, 10),
->>>>>>> 801af6f (expire tests done)
 				"EXPIRE test_key " + strconv.FormatInt(10, 10) + " LT",
 			},
 			expected: []interface{}{"OK", 1, 1},
@@ -139,12 +126,17 @@ func TestEXPIRE(t *testing.T) {
 			},
 			expected: []interface{}{"OK", 1,
 <<<<<<< HEAD
+<<<<<<< HEAD
 				errors.New("NX and XX, GT or LT options at the same time are not compatible"),
 				errors.New("NX and XX, GT or LT options at the same time are not compatible"),
 =======
 				errors.New("ERR NX and XX, GT or LT options at the same time are not compatible"),
 				errors.New("ERR NX and XX, GT or LT options at the same time are not compatible"),
 >>>>>>> 801af6f (expire tests done)
+=======
+				errors.New("NX and XX, GT or LT options at the same time are not compatible"),
+				errors.New("NX and XX, GT or LT options at the same time are not compatible"),
+>>>>>>> 1b05d06 (all test case done except case-sesintive)
 				"test_value"},
 		},
 		{
@@ -193,12 +185,16 @@ func TestEXPIRE(t *testing.T) {
 				"EXPIRE test_key " + strconv.FormatInt(1, 10) + " nx" + " " + "xx" + " " + "lt",
 			},
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1b05d06 (all test case done except case-sesintive)
 			expected: []interface{}{"OK", errors.New("unsupported option rr"),
 				errors.New("NX and XX, GT or LT options at the same time are not compatible"),
 				errors.New("GT and LT options at the same time are not compatible"),
 				errors.New("GT and LT options at the same time are not compatible"),
 				errors.New("NX and XX, GT or LT options at the same time are not compatible"),
 				errors.New("NX and XX, GT or LT options at the same time are not compatible"),
+<<<<<<< HEAD
 				errors.New("NX and XX, GT or LT options at the same time are not compatible"),
 			},
 		},
@@ -217,6 +213,9 @@ func TestEXPIRE(t *testing.T) {
 				errors.New("ERR NX and XX, GT or LT options at the same time are not compatible"),
 				errors.New("ERR NX and XX, GT or LT options at the same time are not compatible")},
 >>>>>>> 801af6f (expire tests done)
+=======
+				errors.New("NX and XX, GT or LT options at the same time are not compatible")},
+>>>>>>> 1b05d06 (all test case done except case-sesintive)
 		},
 	}
 	runTestcases(t, client, testCases)
