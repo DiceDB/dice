aa67c3eeac45faefd35b0c680e91ca7c9e8010f0
Arpit <arpit.b.bhayani@gmail.com>
2025-03-01 15:14:23 +0000

    Documentation of HANDSHAKE command

--- internal/cmd/cmd_handshake.go
+++ internal/cmd/cmd_handshake.go
@@ -11,9 +11,29 @@ import (
 
 var cHANDSHAKE = &CommandMeta{
 	Name:      "HANDSHAKE",
-	HelpShort: "HANDSHAKE is used to handshake with the database; sends client_id and execution mode",
-	Eval:      evalHANDSHAKE,
-	Execute:   executeHANDSHAKE,
+	Syntax:    "HANDSHAKE <client_id> <execution_mode>",
+	HelpShort: "HANDSHAKE tells the server the purpose of the connection",
+	HelpLong: `
+HANDSHAKE is used to tell the DiceDB server the purpose of the connection. It
+registers the client_id and execution_mode.
+
+The client_id is a unique identifier for the client. It can be any string, typically
+a UUID.
+
+The execution_mode is the mode of the connection, it can be one of the following:
+
+1. "command" - The client will send commands to the server and receive responses.
+2. "watch" - The connection in the watch mode will be used to receoive the responses of query subscriptions.
+
+If you use DiceDB SDK or CLI then this HANDSHAKE command is automatically sent when the connection is established
+or when you establish a subscription.
+	`,
+	Examples: `
+localhost:7379> HANDSHAKE 4c9d0411-6b28-4ee5-b78a-e7e258afa52f command
+OK OK
+	`,
+	Eval:    evalHANDSHAKE,
+	Execute: executeHANDSHAKE,
 }
 
 func init() {
