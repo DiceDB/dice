98f70f9fefff259e10bf3176cb973fbb1ba3ee5d
aasifkhan7 <aasifk106@gmail.com>
2025-03-03 23:06:11 +0530

    Update getex_test.go (#1546)
    * Update getex_test.go
    
    * Update getex_test.go

--- tests/commands/ironhawk/getex_test.go
+++ tests/commands/ironhawk/getex_test.go
@@ -4,6 +4,7 @@
 package ironhawk
 
 import (
+	"errors"
 	"strconv"
 	"testing"
 	"time"
@@ -12,93 +13,81 @@ import (
 func TestGETEX(t *testing.T) {
 	client := getLocalConnection()
 	defer client.Close()
-	Etime5 := strconv.FormatInt(time.Now().Unix()+5, 10)
-	Etime10 := strconv.FormatInt(time.Now().Unix()+10, 10)
 
 	testCases := []TestCase{
 		{
 			name:     "GETEX Simple Value",
 			commands: []string{"SET foo bar", "GETEX foo", "GETEX foo", "TTL foo"},
 			expected: []interface{}{"OK", "bar", "bar", -1},
-			delay:    []time.Duration{0, 0, 0, 0},
 		},
 		{
 			name:     "GETEX Non-Existent Key",
 			commands: []string{"GETEX nonExecFoo", "TTL nonExecFoo"},
 			expected: []interface{}{nil, -2},
-			delay:    []time.Duration{0, 0},
 		},
 		{
 			name:     "GETEX with EX option",
-			commands: []string{"SET foo bar", "GETEX foo", "TTL foo", "GETEX foo ex 2", "TTL foo", "GETEX foo", "TTL foo"},
-			expected: []interface{}{"OK", "bar", -1, "bar", 2, nil, -2},
-			delay:    []time.Duration{0, 0, 0, 0, 0, 2 * time.Second, 0},
+			commands: []string{"SET foo bar", "GETEX foo EX 2", "TTL foo", "GET foo", "GET foo"},
+			expected: []interface{}{"OK", "bar", 2, "bar", nil},
+			delay:    []time.Duration{0, 0, 0, 0, 3 * time.Second},
 		},
 		{
 			name:     "GETEX with PX option",
-			commands: []string{"SET foo bar", "GETEX foo", "TTL foo", "GETEX foo px 2000", "TTL foo", "GETEX foo", "TTL foo"},
-			expected: []interface{}{"OK", "bar", -1, "bar", 2, nil, -2},
-			delay:    []time.Duration{0, 0, 0, 0, 0, 2 * time.Second, 0},
+			commands: []string{"SET foo bar", "GETEX foo PX 2000", "TTL foo", "GET foo", "GET foo"},
+			expected: []interface{}{"OK", "bar", 2, "bar", nil},
+			delay:    []time.Duration{0, 0, 0, 0, 3 * time.Second},
 		},
 		{
-			name:     "GETEX with EX option and invalid value",
-			commands: []string{"SET foo bar", "GETEX foo", "TTL foo", "GETEX foo ex -1", "TTL foo", "GETEX foo", "TTL foo"},
-			expected: []interface{}{"OK", "bar", -1, "ERR invalid expire time in 'getex' command", -1, "bar", -1},
-			delay:    []time.Duration{0, 0, 0, 0, 0, 0, 0},
+			name:     "GETEX with PERSIST option",
+			commands: []string{"SET foo bar EX 100", "TTL foo", "GETEX foo PERSIST", "TTL foo"},
+			expected: []interface{}{"OK", 100, "bar", -1},
+		},
+
+		{
+			name:     "GETEX with invalid option",
+			commands: []string{"SET foo bar", "GETEX foo INVALID"},
+			expected: []interface{}{"OK", "bar"},
 		},
 		{
-			name:     "GETEX with PX option and invalid value",
-			commands: []string{"SET foo bar", "GETEX foo", "TTL foo", "GETEX foo px -1", "TTL foo", "GETEX foo", "TTL foo"},
-			expected: []interface{}{"OK", "bar", -1, "ERR invalid expire time in 'getex' command", -1, "bar", -1},
-			delay:    []time.Duration{0, 0, 0, 0, 0, 0, 0},
+			name:     "GETEX with negative expiry",
+			commands: []string{"SET foo bar", "GETEX foo EX -1"},
+			expected: []interface{}{
+				"OK",
+				errors.New("invalid value for a parameter in 'GETEX' command for EX parameter"),
+			},
 		},
 		{
-			name:     "GETEX with EXAT option",
-			commands: []string{"SET foo bar", "GETEX foo", "TTL foo", "GETEX foo exat " + Etime5, "TTL foo", "GETEX foo", "TTL foo"},
-			expected: []interface{}{"OK", "bar", -1, "bar", 5, nil, -2},
-			delay:    []time.Duration{0, 0, 0, 0, 0, 5 * time.Second, 0},
+			name:     "GETEX with zero expiry",
+			commands: []string{"SET foo bar", "GETEX foo PX 0"},
+			expected: []interface{}{
+				"OK",
+				errors.New("invalid value for a parameter in 'GETEX' command for PX parameter"),
+			},
+		},
+		{
+			name:     "GETEX with non-numeric expiry",
+			commands: []string{"SET foo bar", "GETEX foo EX abc"},
+			expected: []interface{}{
+				"OK",
+				errors.New("invalid value for a parameter in 'GETEX' command for EX parameter"),
+			},
 		},
 		{
 			name:     "GETEX with PXAT option",
-			commands: []string{"SET foo bar", "GETEX foo", "TTL foo", "GETEX foo pxat " + Etime10 + "000", "TTL foo", "GETEX foo", "TTL foo"},
-			expected: []interface{}{"OK", "bar", -1, "bar", 5, nil, -2},
-			delay:    []time.Duration{0, 0, 0, 0, 0, 5 * time.Second, 0},
+			commands: []string{
+				"SET foo bar",
+				"GETEX foo PXAT " + strconv.FormatInt(time.Now().Add(24*time.Hour).UnixMilli(), 10),
+			},
+			expected: []interface{}{"OK", "bar", 2, "bar", nil},
+			delay:    []time.Duration{0, 0, 0, 0, 3 * time.Second},
 		},
 		{
-			name:     "GETEX with EXAT option and invalid value",
-			commands: []string{"SET foo bar", "GETEX foo", "TTL foo", "GETEX foo exat 123123", "GETEX foo", "TTL foo"},
-			expected: []interface{}{"OK", "bar", -1, "bar", nil, -2},
-			delay:    []time.Duration{0, 0, 0, 0, 0, 0},
-		},
-		{
-			name:     "GETEX with PXAT option and invalid value",
-			commands: []string{"SET foo bar", "GETEX foo", "TTL foo", "GETEX foo pxat 123123", "GETEX foo", "TTL foo"},
-			expected: []interface{}{"OK", "bar", -1, "bar", nil, -2},
-			delay:    []time.Duration{0, 0, 0, 0, 0, 0},
-		},
-		{
-			name:     "GETEX with Persist option",
-			commands: []string{"SET foo bar", "GETEX foo ex 2", "TTL foo", "GETEX foo persist", "TTL foo"},
-			expected: []interface{}{"OK", "bar", 2, "bar", -1},
-			delay:    []time.Duration{0, 0, 0, 0, 0},
-		},
-		{
-			name:     "GETEX with multiple expiry options",
-			commands: []string{"SET foo bar", "GETEX foo ex 2 px 123123", "TTL foo", "GETEX foo"},
-			expected: []interface{}{"OK", "ERR syntax error", -1, "bar"},
-			delay:    []time.Duration{0, 0, 0, 0},
-		},
-		{
-			name:     "GETEX with persist and ex options",
-			commands: []string{"SET foo bar", "GETEX foo ex 2", "TTL foo", "GETEX foo persist ex 2", "TTL foo"},
-			expected: []interface{}{"OK", "bar", 2, "ERR syntax error", 2},
-			delay:    []time.Duration{0, 0, 0, 0, 0},
-		},
-		{
-			name:     "GETEX with persist and px options",
-			commands: []string{"SET foo bar", "GETEX foo px 2000", "TTL foo", "GETEX foo px 2000 persist", "TTL foo"},
-			expected: []interface{}{"OK", "bar", 2, "ERR syntax error", 2},
-			delay:    []time.Duration{0, 0, 0, 0, 0},
+			name:     "GETEX with past EXAT timestamp",
+			commands: []string{"SET foo bar", "GETEX foo EXAT " + strconv.FormatInt(time.Now().Add(-1*time.Hour).Unix(), 10)},
+			expected: []interface{}{
+				"OK",
+				errors.New("invalid value for a parameter in 'GETEX' command for EXAT parameter"),
+			},
 		},
 	}
 
