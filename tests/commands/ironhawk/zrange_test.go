de604e599ce0ffedf8884e22bee18e80b9040a02
Vivek Kothari <5404922+vivekkothari@users.noreply.github.com>
2025-04-08 18:30:39 +0530

    Migrate zrange (#1616)
    * migrate zrange
    
    * Add tests
    
    * Add more tests

---
+++ tests/commands/ironhawk/zrange_test.go
@@ -0,0 +1,51 @@
+package ironhawk
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestZrange(t *testing.T) {
+	client := getLocalConnection()
+	defer client.Close()
+
+	testCases := []TestCase{
+		{
+			name:     "Test ZRANGE command with bad params",
+			commands: []string{"ZRANGE", "ZRANGE key", "ZRANGE key 1"},
+			expected: []interface{}{errors.New("wrong number of arguments for 'ZRANGE' command"), errors.New("wrong number of arguments for 'ZRANGE' command"), errors.New("wrong number of arguments for 'ZRANGE' command")},
+		},
+		{
+			name:     "Test ZRANGE command non numeric start and stop",
+			commands: []string{"ZRANGE key a b", "ZRANGE key 1 b"},
+			expected: []interface{}{errors.New("value is not an integer or a float"), errors.New("value is not an integer or a float")},
+		},
+		{
+			name:     "Test ZRANGE command non existent key",
+			commands: []string{"ZRANGE key 1 2"},
+			expected: []interface{}{nil},
+		},
+		{
+			name:     "Test ZRANGE on non sorted set key",
+			commands: []string{"SET key value", "ZRANGE key 1 2"},
+			expected: []interface{}{"OK", errors.New("wrongtype operation against a key holding the wrong kind of value")},
+		},
+		{
+			name: "Test ZRANGE on set",
+			commands: []string{
+				"ZADD sorted_set 1 mem1 2 mem2",
+				"ZRANGE sorted_set 0 1",
+				"ZRANGE sorted_set 0 1 REV",
+				"ZRANGE sorted_set 0 1 WITHSCORES",
+				"ZRANGE sorted_set 0 1 REV WITHSCORES"},
+			expected: []interface{}{
+				int64(2),
+				[]string{"mem1", "mem2"},
+				[]string{"mem2", "mem1"},
+				[]string{"mem1", "1", "mem2", "2"},
+				[]string{"mem2", "2", "mem1", "1"}},
+		},
+	}
+
+	runTestcases(t, client, testCases)
+}
