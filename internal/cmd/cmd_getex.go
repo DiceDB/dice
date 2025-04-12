61df857a9e7708cbfc022dada22c4875166dc01d
Arpit <arpit.b.bhayani@gmail.com>
2025-04-12 15:25:09 +0000

    GETEX Documentation updated with apt CLI output

--- internal/cmd/cmd_getex.go
+++ internal/cmd/cmd_getex.go
@@ -36,11 +36,11 @@ The command returns (nil) if the key does not exist. The command supports the fo
 	`,
 	Examples: `
 localhost:7379> SET k v
-OK OK
+OK
 localhost:7379> GETEX k EX 1000
 OK v
 localhost:7379> TTL k
-OK 996
+OK 995
 localhost:7379> GETEX k PX 200000
 OK v
 localhost:7379> GETEX k EXAT 1772377267
@@ -49,8 +49,8 @@ localhost:7379> GETEX k PXAT 1772377267000
 OK v
 localhost:7379> GETEX k PERSIST
 OK v
-localhost:7379> GET k
-(nil)
+localhost:7379> EXPIRETIME k
+OK -1
 	`,
 	Eval:    evalGETEX,
 	Execute: executeGETEX,
